4cb6fc4e91a76e6f713de291981d309e83670a60
roronya <roronya628@gmail.com>
2018-08-10 02:00:30 +0900

    use final form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Form, Field} from 'react-final-form'
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+const onSubmit = async values => {
+	await sleep(300);
+	window.alert(JSON.stringify(values));
+};
 
 class App extends Component {
   render() {
@@ -13,6 +21,36 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+				<button
+					type="submit"
+					onClick={() =>
+						document
+							.getElementById("exampleForm")
+							.dispatchEvent(new Event("submit"))
+					}
+				>
+					External Submit via <code>document.getElementById()</code>
+				</button>
+				<Form
+					onSubmit={onSubmit}
+					initialValues={{firstName: "hogehoge"}}
+					render={({handleSubmit, form, values}) => (
+						<form onSubmit={handleSubmit} id="exampleForm">
+							<div>
+								<label>First Name</label>
+								<Field
+									name="firstName"
+									component="input"
+									type="text"
+									placeholder="First Name"
+									/>
+							</div>
+							<button type="submit">
+								Submit
+							</button>
+						</form>
+					)}
+				/>
       </div>
     );
   }
